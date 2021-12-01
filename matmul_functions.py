3b951c503a32b5e4290fbbbe774097b75015086e
elialter <63195898+elialter@users.noreply.github.com>
2021-12-01 15:19:46 +0200

    part 3 implementation

--- matmul_functions.py
+++ matmul_functions.py
@@ -4,20 +4,51 @@ import timeit
 
 
 def matmul_transpose_trivial(X):
-    raise NotImplementedError("To be implemented")
-
+    result = np.zeros((len(X), len(X)), dtype=np.float64)
+    for i in range(0, len(X)):
+        for k in range(0, len(X[0])):
+            for j in range(0, len(X)):
+                result[i][j] += X[i][k] * X[j][k]
+    return result
 
 @njit
 def matmul_transpose_numba(X):
-    raise NotImplementedError("To be implemented")
+    result = np.zeros((X.shape[0], X.shape[0]), dtype=np.float64)
+    for i in range(0, X.shape[0]):
+        for j in range(0, i+1):
+            partial_sum = 0
+            for k in range(0, X.shape[1]):
+                partial_sum += X[i][k] * X[j][k]
+            result[i][j] = partial_sum
+            result[j][i] = partial_sum
+
+    return result
 
 
 def matmul_transpose_gpu(X):
-    raise NotImplementedError("To be implemented")
+    resultArray = np.zeros((X.shape[0], X.shape[0]), dtype=np.float64)
+    X_gpu = cuda.to_device(X)
+    resultArr_gpu = cuda.to_device(resultArray)
+    matmul_kernel[1, 1024](X_gpu, resultArr_gpu)
+    resultArray = resultArr_gpu.copy_to_host(resultArray)
+    return resultArray
 
 @cuda.jit
 def matmul_kernel(A, C):
-    raise NotImplementedError("To be implemented")
+    i = cuda.threadIdx.x
+
+    for threadNum in range(i, C.shape[0] * C.shape[0], 1024):
+        row = threadNum // C.shape[1]
+        col = threadNum % C.shape[1]
+        arr = 0
+        if row > col:
+            continue
+
+        for z in range(0, A.shape[1]):
+            arr += A[row, z] * A[col, z]
+        cuda.atomic.add(C[row], col, arr)
+        cuda.atomic.add(C[col], row, arr)
+    cuda.syncthreads()
 
 #this is the comparison function - keep it as it is, don't change X or Y.
 def matmul_comparison():
