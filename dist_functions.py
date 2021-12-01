b151378cb3387ff1a1ece3b11cae8b15ad069e4b
elialter <63195898+elialter@users.noreply.github.com>
2021-12-01 12:50:10 +0200

    gpu_dist implementation

--- dist_functions.py
+++ dist_functions.py
@@ -37,17 +37,35 @@ def dist_numba(A, B, p):
     return sum**(1 / p)
 
 def dist_gpu(A, B, p):
-    """
-     Returns
-     -------
-     np.array
-         p-dist between A and B
-    """
-    pass
+    C = np.zeros(1, np.float64)
+
+    gpuA = cuda.to_device(A)
+    gpuB = cuda.to_device(B)
+    gpuC = cuda.to_device(C)
+
+    dist_kernel[1000, 1000](gpuA, gpuB, p, gpuC)
+    C = gpuC.copy_to_host(C)
+    return C[0] ** (1.0 / p)
+
 
 @cuda.jit
 def dist_kernel(A, B, p, C):
-   pass
+    i = cuda.threadIdx.x
+    j = cuda.blockIdx.x
+    s_arr = cuda.shared.array(1, float32)
+
+    if i == 0:
+        s_arr[0] = 0.0
+    cuda.syncthreads()
+
+    if i < 1000 and j < 1000:
+        thread_sum = abs(A[i][j] - B[i][j]) ** p
+        cuda.atomic.add(s_arr, 0, thread_sum)
+    cuda.syncthreads()
+
+    if i == 0:
+        cuda.atomic.add(C, 0, s_arr[0])
+
    
 #this is the comparison function - keep it as it is.
 def dist_comparison():
